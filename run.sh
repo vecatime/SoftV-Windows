046372b32b2b44c84e44bd9b55242956349f2aa4
Mohsen Sedaghat <m.sedaghat9414@gmail.com>
2023-11-24 13:42:47 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,3 +12,31 @@ chmod 600 *
 chmod 700 vpnserver vpncmd
 ./vpnserver start
 ./vpncmd
+echo "1\n"
+echo "ServerPasswordSet\n"
+
+sudo cat >> /lib/systemd/system/vpnserver.service << EOF
+[Unit]
+Description=SoftEther VPN Server
+After=network.target
+[Service]
+Type=forking
+ExecStart=
+ExecStart=/usr/local/vpnserver/vpnserver start
+ExecStop=/usr/local/vpnserver/vpnserver stop
+[Install]
+WantedBy=multi-user.target
+EOF
+echo "\n"
+echo net.ipv4.ip_forward = 1 | ${SUDO} tee -a /etc/sysctl.conf
+systemctl enable vpnserver
+systemctl start vpnserver
+systemctl status vpnserver
+ufw allow 443
+ufw allow 500,4500/udp
+ufw allow 1701
+ufw allow 1194
+ufw allow 5555
+systemctl enable vpnserver
+systemctl start vpnserver
+systemctl status vpnserver
